84e4e680bdb90d51b3c4869130c8bb128c05fd8b
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-17 00:49:12 -0400

    Removendo projeto errado e adicionando novo

--- pessoa/src/Pessoa.kt
+++ pessoa/src/Pessoa.kt
@@ -6,7 +6,7 @@ open class Pessoa(val nome: String, val idade: Int) {
             println("Olá, ${nome}! Você tem ${idade} anos, logo, já pode trabalhar no Pela Bank!")
         } else {
             val anosFaltando = 18 - idade
-            println("Olá, ${nome}! Você tem ${idade} anos, logo, ainda faltam ${anosFaltando} anos para você poder trabalhar no Pela Bank...")
+            println("Olá, ${nome}! Você tem ${idade} anos, logo, ainda falta(m) ${anosFaltando} ano(s) para você poder trabalhar no Pela Bank...")
         }
     }
 }
